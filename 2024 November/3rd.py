0cd583a0d72b9616fe4e16b46f632fb5a6150758
eunseo <139933415+froggyjuice@users.noreply.github.com>
2024-11-03 16:14:24 +0900

    Create 3rd.py
    05-2 함수 고급

---
+++ 2024 November/3rd.py
@@ -0,0 +1,76 @@
+a = [1, 2, 3]
+
+def function():
+    print(a)
+    a.append(4)
+    print(a)
+
+function()
+function()
+
+def power(item):
+    return item ** 2
+def under_3(item):
+    return item < 3
+
+list_input_a = [1, 2, 3, 4, 5]
+
+output_a = map(power, list_input_a)
+print('# map 함수의 실행결과')
+print(f'map(power, list_input_a): {output_a}')
+print(f'map(power, list_input_a): {list(output_a)}')
+print()
+
+# 예상결과 첫 번째 줄 map 주소// 두 번째 줄 [1, 4, 9, 16, 25]
+
+output_b = filter(under_3, list_input_a)
+print('# filter 함수의 실행결과')
+print(f'filter(under_3, list_input_a): {output_b}')
+print(f'filter(under_3, list_input_a): {list(output_b)}')
+print()
+
+# 예상결과 첫 번째 줄 filter 주소// 두 번째 줄 [1, 2]
+
+def my_map(콜백함수, 리스트):
+    output = []
+    for 요소 in 리스트:
+        output.append(콜백함수(요소))
+    return output
+
+def power(숫자):
+    return 숫자 ** 2
+
+A = [1, 2, 3, 4, 5]
+print(my_map(power, A))
+
+def my_filter(콜백함수, 리스트):
+    output = []
+    for 요소 in 리스트:
+        if 콜백함수(요소):
+            output.append(요소)
+    return output
+
+def under_3(숫자):
+    return 숫자 < 3
+      
+print(my_filter(under_3, A))
+
+is_odd = lambda 숫자: 숫자 % 2 == 1
+print(is_odd(10))
+
+sum_ab = lambda a, b: a + b
+print(sum_ab(3,5))
+
+import random
+
+family_name_list = list("김이박송원문주")
+hanguls = list("가나다라마바사아자차카타파하")
+
+with open("data.txt", "w") as file:
+    file.write("name, height, weight\n")
+    for i in range(1000):
+        weight = random.randrange(40, 120)
+        height = random.randrange(150, 200)
+        first_name = random.choice(hanguls) + random.choice(hanguls)
+        last_name = random.choice(family_name_list)
+        name = last_name + first_name
