50abf1127e79e331717454eb5b18cb1bd8da44cf
eunseo <139933415+froggyjuice@users.noreply.github.com>
2024-11-10 00:00:14 +0900

    Update 9th.py

--- 2024 November/9th.py
+++ 2024 November/9th.py
@@ -1,1 +1,39 @@
+while True:
+    try:
+        user_input = input()
+        if not user_input:  # 입력이 없으면 종료
+            break
+        print(user_input)  # 입력이 있으면 그대로 출력
+    except:
+        break
+      
+while True:
+    user_input = input()
+    if not user_input:
+        break
+    print(user_input)
+
+S = input()  # 문자열 입력
+alphabet_list = 'abcdefghijklmnopqrstuvwxyz'  # 알파벳 리스트
+
+# 각 알파벳에 대해 첫 등장 위치 또는 -1 출력
+for alphabet in alphabet_list:
+    if alphabet in S:
+        print(S.index(alphabet), end=' ')  # 첫 번째로 등장하는 위치 출력
+    else:
+        print(-1, end=' ')  # 알파벳이 없다면 -1 출력
+
+N = int(input())
+myinput = input()
+
+if len(myinput) == N:
+    output = 00
+    for char in myinput:
+        output += int(char)
+else:
+    pass
+
+print(output)
+
+print(ord(input()))
 
